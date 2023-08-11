506214acc48a8709791c33ab55035a5ddf6a1993
Maaz Jamshaid <81762527+maazjamshaid123@users.noreply.github.com>
2023-08-11 16:20:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
-# Import necessary libraries
 import streamlit as st
-import numpy as np
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
 import joblib
 
-# Load the ShuffleSplit best models
+# Load the saved best models
 n_ss_splits = 20
-best_models = [joblib.load(f"saved_svr_model_{i}.joblib") for i in range(n_ss_splits)]
+best_models = []
+for i in range(n_ss_splits):
+    model_filename = f"best_model_svr_{i}.joblib"  # Adjust the filename format
+    best_models.append(joblib.load(model_filename))
 
-# Define a function to make predictions using each best model
-def predict_multiple_models(input_data, best_models):
-    predictions = [model.predict(input_data) for model in best_models]
-    return predictions
+# Create a Streamlit web app
+st.title("SVR Model Prediction with Best Models")
 
-# Create the Streamlit web app
-st.title("SVR Model Predictions")
+# Input form for user to provide 4 input features
+st.write("Enter the input features:")
+input_features = []
+for i in range(4):
+    feature = st.number_input(f"Feature {i+1}", value=0.0, format="%.2f")
+    input_features.append(feature)
 
-# Input form for new entry
-st.header("Input for Prediction")
-input_features = st.text_input("Input features (comma-separated values)", "1,2,3,4")
-input_data = np.array([list(map(float, input_features.split(",")))])  # Convert input to a 2D array
+# Normalize the input features
+scaler = StandardScaler()
+input_features_scaled = scaler.fit_transform([input_features])
 
-# Predictions using each best model from ShuffleSplit
-multiple_models_predictions = predict_multiple_models(input_data, best_models)
+# Make predictions using each best model
+predictions = np.array([model.predict(input_features_scaled) for model in best_models])
 
-# Calculate average and standard deviation of predictions
-predictions_array = np.array(multiple_models_predictions)
-average_predictions = np.mean(predictions_array, axis=0)
-std_dev_predictions = np.std(predictions_array, axis=0)
+# Display predictions for each best model
+st.write("Predictions from each best model:")
+for i, pred in enumerate(predictions):
+    st.write(f"Best Model {i+1}: {pred[0]:.2f}")
 
-# Display the results
-st.subheader("Predictions using each best model from ShuffleSplit")
-st.write("Individual Predictions:", multiple_models_predictions)
-st.write("Average Prediction:", average_predictions[0])
-st.write("Standard Deviation of Predictions:", std_dev_predictions[0])
+# Calculate and display the average and standard deviation of predictions
+average_pred = np.mean(predictions)
+std_deviation_pred = np.std(predictions)
+st.write(f"Average Prediction: {average_pred:.2f}")
+st.write(f"Standard Deviation: {std_deviation_pred:.2f}")
